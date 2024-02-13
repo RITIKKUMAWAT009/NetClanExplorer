d7849e12ab697e46eae5872969bad648c7045d90
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-02-13 17:14:03 +0530

    net clan demo project tab layout added

---
+++ app/src/main/java/com/example/netclanexplorer/MainActivity.java
@@ -0,0 +1,24 @@
+ package com.example.netclanexplorer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+ public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init(){
+        viewPager=findViewById(R.id.viewPager);
+        tabLayout=findViewById(R.id.tabLayout);
+    }
+}
